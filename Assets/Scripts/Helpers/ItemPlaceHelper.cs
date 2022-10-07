ffc1bdcc29d4096e080e7c924d3a128a46779ec6
Eda Arslan <edarslan9996@gmail.com>
2022-10-07 09:57:57 +0300

    build added

--- Assets/Scripts/Helpers/ItemPlaceHelper.cs
+++ Assets/Scripts/Helpers/ItemPlaceHelper.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using static GameEnums;
-using static UnityEditor.Experimental.GraphView.GraphView;
 
 public class ItemPlaceHelper : MonoBehaviour
 {
